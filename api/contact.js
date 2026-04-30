d7102affb819b1d1e35a1edc96fd22e953c2ece2
nawrockipiotr <nawrockipiotr.2@gmail.com>
2026-04-30 14:37:56 +0200

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
       });
     }
 
-    const emailPattern = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(email)) {
       return res.status(400).json({
         success: false,
